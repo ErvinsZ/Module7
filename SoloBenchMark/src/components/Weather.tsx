dd4bd1a79027f7779a9c8379107b65fbefc51d55
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-08-07 17:06:07 +0300

    Update Weather.tsx

--- SoloBenchMark/src/components/Weather.tsx
+++ SoloBenchMark/src/components/Weather.tsx
@@ -26,7 +26,8 @@ class Weather extends React.Component<IWeatherProps, IWeatherStates> {
                 {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
                 {this.props.temperature && <p>Temperature: {this.props.temperature} °C</p>}
                 {this.props.humidity && <p>Humidity: {this.props.humidity}<WiHumidity style={{paddingBottom:"7px", fontSize:"40px"}}/></p>}
-                {this.props.description && <p>Conditions: {this.props.description}</p>}
+                {this.props.description && <p style={{textTransform: "capitalize"}}>Conditions: {this.props.description}
+</p>}
                 {this.props.error && <p>{this.props.error} </p>}
         
             </div>
